867f5e521833c8c309d2bc222d9d1892be61f7ad
andersonpassos0 <anderson.passos@yahoo.com.br>
2022-12-28 17:11:37 -0300

    implementa contrato da API

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/cliente")
+public interface ClienteAPI {
+	
+	ClienteResponse postCliente(ClienteRequest clienteRequest);
+
+}
